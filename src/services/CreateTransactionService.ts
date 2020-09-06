6e706004d67735a48c2917e7f812e6aeef3cb6da
Fellipe Santana <fellipe.santana@gmail.com>
2020-09-06 19:16:45 -0300

    Implementacao do desafio fundamentos de nodejs

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if (value <= 0) {
+      throw new Error('The value must be higher than zero.');
+    }
+
+    if (type === 'outcome' && value > balance.total) {
+      throw new Error('Transaction cannot be completed. Insufficient funds.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
